20a6837e471fd48e2749ca854a151071d2edaa3f
alexandrapierson <alyx.pierson@gmail.com>
2023-11-12 17:41:10 -0600

    added axios to data page

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   let userData = await axios.get('https://jsonplaceholder.typicode.com/users');
-  
+
   res.render('data',
     {
       userData: userData.data,

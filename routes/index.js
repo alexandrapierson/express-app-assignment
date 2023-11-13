ec1623ec0fd4826195ab6c4ac03790b91b579b3d
alexandrapierson <alyx.pierson@gmail.com>
2023-11-12 18:50:23 -0600

    added an event listener for submit

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('login');
+
+  form.addEventListening('submit', function(event) {
+    event.preventDefault();
+
+    window.location.href = '/data';
+  });
+});
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,5 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET users listing. */
-router.get('/list', function(req, res, next) {
-  res.send('Heres a list of users');
-});
 
 module.exports = router;
